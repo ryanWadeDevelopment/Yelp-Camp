0d974f822b3279e01c1218bd9d5c6bd29f2b0f83
Ryan Wade <ryanwadedevelopment@gmail.com>
2022-10-26 17:44:54 -0500

    415 Create and New

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ db.once("open", () => {
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
@@ -26,6 +28,16 @@ app.get("/", (req, res) => {
 app.get("/campgrounds", async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render("campgrounds/index", { campgrounds });
+});
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get("/campgrounds/:id", async (req, res) => {
